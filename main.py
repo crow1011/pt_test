fa80e520a575e19553d5b4c31faced36539f9f8d
crow1011 <crowsmith1011>
2021-02-10 21:40:12 +0300

    add comments

--- main.py
+++ main.py
@@ -2,21 +2,26 @@
 from ipaddress import ip_address, ip_network
 import argparse
 
+# включение и отключение дебага
 debug = False
 
 
 
 def get_net_list(fname):
 	res = []
+	# читаем файл и разбиваем по строкам
 	with open(fname, 'r') as f:
 		net_list = f.read().split('\n')
+	# инкапсулируем строковый адрес в объект ip_network
 	for net in net_list:
 		try:
 			if '/' in net:
 				res.append(ip_network(net))
 			else:
+				# добавляем маску если адрес одиночный
 				res.append(ip_network(net+'/32'))
 		except ValueError:
+			# если попадется невалидный адрес в списке, он будет проигнорирован
 			print('Missing invalid address(ignore):', net)
 	return res
 
@@ -25,36 +30,44 @@ def get_net_list(fname):
 def save_results(res, report_path):
 	with open(report_path, 'w') as f:
 		for net in res:
+			# пишет построчно из генератора
 			f.write(str(net) + '\n')
 
 
 def net_filter(n_allow, n_deny, only_24_32):
+	# подготавливаем список для результатов
 	res = []
 	for anet in n_allow:
-		# print(anet)
+		# задаем статус для сети из allow списка в False, если статус не изменится, она будет добавлена в res
 		overlaps_status = False
 		for dnet in n_deny:
-			# print(anet.overlaps(dnet), dnet, anet)
+			# проверяем пересечение со всеми сетями из deny
 			if anet.overlaps(dnet):
+				# если пересечение обнаружено меняем статус на False, в res сеть не попадет
 				overlaps_status = True
+				# исключаем пересечение и добавляем результат в конец списка allow, для перепроверки
 				no_overlap = list(anet.address_exclude(dnet))
 				for net in no_overlap:
 					n_allow.append(net)
-				# print(anet.supernet_of(dnet))
-				# print(list(anet.address_exclude(dnet)))
+		# если подсеть не пересекается ни с одной из deny, добавляем в res
 		if not overlaps_status:
 			res.append(anet)
 
 	for rnet in res:
+		# проверяем статус параметра
 		if only_24_32:
+			# если длина префикса 24 - отдаем
 			if rnet.prefixlen==24:
 				yield rnet
+			# если длина префикса 32 - отдаем
 			elif rnet.prefixlen==32:
 				yield rnet
+			# если длина префикса меньше 24 - делим на части по 24 маске и отдаем циклом
 			elif rnet.prefixlen<24:
 				g_net_list = rnet.subnets(new_prefix=24)
 				for g_net in g_net_list:
 					yield g_net
+			# если длина префикса больше 24 - делим на части по 32 маске и отдаем циклом
 			elif rnet.prefixlen>24:
 				g_net_list = rnet.subnets(new_prefix=32)
 				for g_net in g_net_list:
@@ -63,9 +76,12 @@ def net_filter(n_allow, n_deny, only_24_32):
 			yield rnet
 
 def main(allow_list_path, deny_list_path, only_24_32, report_path):
+	# получаем списки deny и allow
 	n_allow = get_net_list(allow_list_path)
 	n_deny = get_net_list(deny_list_path)
+	# создаем генератор с отфильтрованными результатами
 	gres = net_filter(n_allow, n_deny, only_24_32)
+	# сохраняем
 	save_results(gres, report_path)
 
 
@@ -78,9 +94,10 @@ if __name__ == '__main__':
 		only_24_32 = False
 		main(allow_list_path, deny_list_path, only_24_32, report_path)
 	else:
+		# парсим параметры
 		parser = argparse.ArgumentParser(description='Exclude deny networks in allow networks.')
 		parser.add_argument('allow_list', type=str, help='Set path to allow networks list')
-		parser.add_argument('deny_list', type=str, help='Set name for search in data')
+		parser.add_argument('deny_list', type=str, help='Set path to deny networks list')
 		parser.add_argument('-p', action='store_true', help='Only 24 or 32 network mask. Default: False', default=False)
 		parser.add_argument('-o', type=str, help='Path to save report. Default: report.list', default='report.list')
 		args = parser.parse_args()
